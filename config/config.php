dbf4bcfd10c002884e3576943cc26a5e7ec1987f
Joel Alphonso <admin@joeldesign.ca>
2016-10-07 14:45:39 -0400

    Boilerplate update
    Changes :
    - Relocated sources to vendor charcoal-city
    - Added templates startup (to be completed)
    - added city.json config file
    - added routes.json file

--- config/config.php
+++ config/config.php
@@ -18,28 +18,33 @@ setlocale(LC_ALL, 'fr_FR.UTF-8', 'french');
 $this['base_path'] = dirname(__DIR__).'/';
 
 /** Import core settings */
-$this->addFile(__DIR__ . '/admin.json');
-$this->addFile(__DIR__ . '/config.json');
+$this->addFile(__DIR__.'/admin.json');
+$this->addFile(__DIR__.'/config.json');
+$this->addFile(__DIR__.'/city.json');
+
+// Load routes
+$this->addFile(__DIR__.'/routes.json');
+$this->addFile(__DIR__.'/../vendor/beneroch/charcoal-utils/config/routes.json');
 
 /**
  * Load environment settings; such as database credentials
  * or credentials for 3rd party services.
  */
 $app_environment = getenv('APPLICATION_ENV');
-if ( ! $app_environment ) {
+if (!$app_environment) {
     $app_environment = 'local';
 }
 
-if ( isset($app_environment) && $app_environment ) {
+if (isset($app_environment) && $app_environment) {
     $environment = preg_replace('/[^A-Za-z0-9_]+/', '', $app_environment);
 
     /** Import local settings */
-    $exts = [ 'php', 'json', 'yml', 'yaml', 'ini' ];
-    while ( $exts ) {
+    $exts = ['php', 'json', 'yml', 'yaml', 'ini'];
+    while ($exts) {
         $ext = array_pop($exts);
         $cfg = sprintf('%1$s/config.%2$s.%3$s', __DIR__, $environment, $ext);
 
-        if ( file_exists($cfg) ) {
+        if (file_exists($cfg)) {
             $this->addFile($cfg);
             break;
         }
